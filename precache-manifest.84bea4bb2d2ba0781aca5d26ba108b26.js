540a90d44678d6f64a98798547dd63dc5bb36254
DagmawiG <dgebreselasse@gmail.com>
2020-09-17 00:06:39 -0500

    Updates

--- precache-manifest.84bea4bb2d2ba0781aca5d26ba108b26.js
+++ precache-manifest.84bea4bb2d2ba0781aca5d26ba108b26.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8d57a49471c0492106edadc257e1e111",
+    "revision": "2e8fc1d796ab428643318fbe0246f238",
     "url": "/visual-memory-game/index.html"
   },
   {
-    "revision": "447259864e1fcca021bc",
+    "revision": "8fd2796ca4f44c0ac350",
     "url": "/visual-memory-game/static/css/main.ebf8f00d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/visual-memory-game/static/js/2.0858b936.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "447259864e1fcca021bc",
-    "url": "/visual-memory-game/static/js/main.433c033a.chunk.js"
+    "revision": "8fd2796ca4f44c0ac350",
+    "url": "/visual-memory-game/static/js/main.094485da.chunk.js"
   },
   {
     "revision": "9da560b31157113ac5fa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/visual-memory-game/precache-manifest.8581f88e0f1905d4298b22af3e6be1a7.js"
+  "/visual-memory-game/precache-manifest.84bea4bb2d2ba0781aca5d26ba108b26.js"
 );
 
 self.addEventListener('message', (event) => {
