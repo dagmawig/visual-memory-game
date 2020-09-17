b2bf87948a683f8e7c3879a93a8b8659498cdaf0
DagmawiG <dgebreselasse@gmail.com>
2020-09-16 23:41:48 -0500

    Updates

---
+++ precache-manifest.281abe02b67b891309d894e975ff0b07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f88fa53006994eb26c8c1a7b4a8af5",
+    "url": "/visual-memory-game/index.html"
+  },
+  {
+    "revision": "7445e92af6a416b58e3d",
+    "url": "/visual-memory-game/static/css/main.ebf8f00d.chunk.css"
+  },
+  {
+    "revision": "fcd0e5e3709eeb0bd08d",
+    "url": "/visual-memory-game/static/js/2.0858b936.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/visual-memory-game/static/js/2.0858b936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7445e92af6a416b58e3d",
+    "url": "/visual-memory-game/static/js/main.ba25e4d7.chunk.js"
+  },
+  {
+    "revision": "9da560b31157113ac5fa",
+    "url": "/visual-memory-game/static/js/runtime-main.ad065220.js"
+  }
+]);
